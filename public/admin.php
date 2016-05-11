0b93928938a3349c732e519f6219d89029db2881
Samad <samad_1309e@aptechgdn.com>
2016-05-12 00:37:33 +0500

    some CSS in linked

--- public/admin.php
+++ public/admin.php
@@ -1,9 +1,27 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Content Managment System</title>
+	<link rel="stylesheet" type="text/css" href="stylesheets/public.css">
 </head>
 <body>
-
+  <div id="header">
+  	<h1>Qazi_Tech</h1>
+  </div>
+  <div id="main">
+  	<div id="navigation">
+  		&nbsp;
+  	</div>
+  	<div id="page">
+  		<h2>Admin Menu</h2>
+  		<p>welcome to the admin area.</p>
+  		<ul>
+  			<li><a href="manage_content.php">Manage Wesite content</a></li>
+  			<li><a href="manage_admins.php">Manage Admin users</a></li>
+  			<li><a href="logout.php">Logout</a></li>
+  		</ul>
+  	</div>
+  </div>
+  <div id="footer">copyright 20xx, Qazi_Tech</div>
 </body>
 </html>
